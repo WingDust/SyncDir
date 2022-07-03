e0f10c0b8f73905f181284344712049691858177
wingdust <995315823@qq.com>
2022-07-03 10:26:17 +0800

    auto-commit

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const fs = require("fs");
 const archiver = require("archiver");
 const ignore = require("ignore");
 
+const filterEmpty = (collection)=>{
+  if (Array.isArray(collection)) {
+    
+    
+  }
+  
+}
+
+
 const gitignore = fs.readFileSync('.gitignore','utf-8').split('\n')
 // const ig = ignore()
 // const ig = ignore().add(['.abc/*', '!.abc/d/'])
