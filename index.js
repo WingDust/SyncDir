a321d02d08c1f26ab2fd14c0a51ddf9eca1f6346
wingdust <995315823@qq.com>
2022-07-03 10:08:40 +0800

    auto-commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const archiver = require("archiver");
 const ignore = require("ignore");
 
-const ig = ignore()
+// const ig = ignore()
+const ig = ignore().add(['.abc/*', '!.abc/d/'])
 console.log(ig)
 
