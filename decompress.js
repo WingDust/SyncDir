a389f1e58c2ea55d21a23c8ef346f604b9f39fd3
wingdust <995315823@qq.com>
2022-07-03 15:27:57 +0800

    auto-commit

--- decompress.js
+++ decompress.js
@@ -1,8 +1,23 @@
-
 const fs = require("fs");
 const ignore = require("ignore");
-const child_process = require('child_process')
+const child_process = require("child_process");
 const http = require("http");
 
+const url = "";
 
-  child_process.spawn(cmd[0],cmd.filter(i=>i!=='7z'))
+const sync = fs.createWriteStream("sync.7z");
+
+const cmd = ['7z','./sync.7z',]
+
+const request = http.get(url, (response) => {
+  response.pipe(sync);
+  sync.on("finish", () => {
+    sync.close();
+    console.log("download finished");
+  });
+});
+
+// child_process.spawn(
+//   cmd[0],
+//   cmd.filter((i) => i !== "7z")
+// );
