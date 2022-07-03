fd9d1183c99598cece747ee59f579b7f4c15ea25
wingdust <995315823@qq.com>
2022-07-03 10:28:58 +0800

    auto-commit

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const fs = require("fs");
 const archiver = require("archiver");
 const ignore = require("ignore");
 
+const captureEmpty = (value,defaultValue)=>{
+  // console.log('val',value,JSON.stringify(value),toRawType(value).includes('HTML'));
+
+  // HTMLSpanElement
+  if (toRawType(value).includes('HTML')) return value
+  // 避免 querySelectorAll 被 JSON.stringify 转成 {}
+  if (toRawType(value).includes('NodeList')&&value.length>0) return value
+  switch (JSON.stringify(value)) {
+    case '':          return defaultValue    
+    case 'null':      return defaultValue        
+    case undefined: return defaultValue             
+    case '{}':        return defaultValue      
+    case '[]':        return defaultValue      
+    case 'false':return defaultValue
+  
+    default: return value
+  }
+}
+
 const filterEmpty = (collection)=>{
   if (Array.isArray(collection)) {
-    
+    let filterDuplicate = new Set(collection)
     
   }
   
 }
+
 
 
 const gitignore = fs.readFileSync('.gitignore','utf-8').split('\n')
